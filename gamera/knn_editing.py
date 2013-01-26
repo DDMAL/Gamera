90db20b85ac6834b767b14cfa0981841de6e859b
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-01-25 19:01:28 -0500

    fix missing/masked imports and reformat knn_editing.py

--- gamera/knn_editing.py
+++ gamera/knn_editing.py
@@ -1,17 +1,17 @@
 #
 # various editing techniques to improve kNN Classifier performance
 # Copyright (C) 2007, 2008 Colin Baumgarten
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -21,17 +21,19 @@ from gamera.args import Args, Int, Check
 from gamera.knn import kNNInteractive
 from gamera.util import ProgressFactory
 
+
 def _randomSetElement(set):
     """Retrieve a random element from the given set
 
     *set*
       The set from which to select a random element"""
-    randPos = randint(0, len(set) - 1);
+    randPos = randint(0, len(set) - 1)
     for pos, elem in enumerate(set):
         if pos == randPos:
             return elem
 
-def _copyClassifier(original, k = 0):
+
+def _copyClassifier(original, k=0):
     """Copy a given kNNClassifer by constructing a new one with identical
 parameters.
 
@@ -48,6 +50,7 @@ parameters.
                 original._perform_splits,
                 k)
 
+
 def _getMainId(classificationResult):
     """Classification results returned from a kNN Classifier are in a list
 containing '(confidence, className)' tuples. So to determine the 'main class',
@@ -56,6 +59,7 @@ the classname with the highest confidence has to be returned.
     # classificationResult is a tuple of the form
     #   ([idname1, idname2, ...], confidences_for_idname1)
     return classificationResult[0][0][1]
+
 
 class AlgoRegistry(object):
     """Registry containing a list of all available editing algorithms. Besides
@@ -78,11 +82,12 @@ let your callable inherit from *EditingAlgorithm* to register your algorithm,"""
         AlgoRegistry.algorithms.append(algoData)
     registerData = staticmethod(registerData)
 
-    def register(name, callable, args = Args(), doc = ""):
+    def register(name, callable, args=Args(), doc=""):
         """Register a new editing Algorithm: The parameters are the same as in
 *AlgoData.__init__*, so see its doc for an explanation of the parameters."""
         AlgoRegistry.registerData(AlgoData(name, callable, args, doc))
     register = staticmethod(register)
+
 
 class AlgoData(object):
     """Class holding all metadata about an editing algorithm, that is required by
@@ -104,7 +109,7 @@ class AlgoData(object):
 *doc*
     A docstring in reStructured Text format, describing the algorithm and its
     parameters"""
-    def __init__(self, name, callable, args = Args(), doc = ""):
+    def __init__(self, name, callable, args=Args(), doc=""):
         self.name = name
         self.callable = callable
         self.args = args
@@ -125,24 +130,25 @@ two properties to the class:
     def __init__(self):
         AlgoRegistry.register(self.name, self, self.args, self.__doc__)
 
+
 class EditMnn(EditingAlgorithm):
     """**edit_mnn** (kNNInteractive *classifier*, int *k* = 0, bool *protectRare*, int *rareThreshold*)
-    
+
 Wilson's *Modified Nearest Neighbour* (MNN, aka *Leave-one-out-editing*).
 The algorithm removes 'bad' glyphs from the classifier, i.e. glyphs that
-are outliers from their class in featurespace, usually because they have been 
+are outliers from their class in featurespace, usually because they have been
 manually misclassified or are not representative for their class
 
     *classifier*
         The classifier from which to create an edited copy
     *internalK*
-        The k value used internally by the editing algorithm. If 0 is given for 
-        this parameter, the original classifier's k is used (recommended).        
+        The k value used internally by the editing algorithm. If 0 is given for
+        this parameter, the original classifier's k is used (recommended).
     *protect rare classes*
         The algorithm tends to completely delete the items of rare classes,
-        removing this whole class from the classifier. If this is not 
+        removing this whole class from the classifier. If this is not
         desired these rare classes can be explicitly protected from deletion.
-        Note that enabling this option causes additional computing effort    
+        Note that enabling this option causes additional computing effort
     *rare class threshold*
         In case *protect rare classes* is enabled, classes with less than this
         number of elements are considered to be rare
@@ -151,12 +157,12 @@ Reference: D. Wilson: 'Asymptotic Properties of NN Rules Using Edited Data'.
 *IEEE Transactions on Systems, Man, and Cybernetics*, 2(3):408-421, 1972
 """
     name = "Wilson's Modified Nearest Neighbour (MNN)"
-    args = Args([Int("Internal k", default = 0),
-                 Check("Protect rare classes", default = True),
-                 Int("Rare class threshold", default = 3)])
+    args = Args([Int("Internal k", default=0),
+                 Check("Protect rare classes", default=True),
+                 Int("Rare class threshold", default=3)])
 
-    def __call__(self, classifier, k = 0, protectRare = True,
-                 rareThreshold = 3):
+    def __call__(self, classifier, k=0, protectRare=True,
+                 rareThreshold=3):
 
         editedClassifier = _copyClassifier(classifier, k)
         toBeRemoved = set()
@@ -176,51 +182,51 @@ Reference: D. Wilson: 'Asymptotic Properties of NN Rules Using Edited Data'.
 
         rareClasses = self._getRareClasses(classifier.get_glyphs(),
                                            protectRare, rareThreshold)
-        
+
         # remove 'bad' glyphs, if they are not in a rare class
         for glyph in toBeRemoved:
             if glyph.get_main_id() in rareClasses:
                 continue
             editedClassifier.get_glyphs().remove(glyph)
-            
+
         progress.kill()
         return editedClassifier
-    
+
     def _getRareClasses(self, glyphs, protectRare, rareThreshold):
         """Produces a set containing the names of all rare classes"""
         result = set()
-        
-        if not protectRare: 
+
+        if not protectRare:
             return result
-        
+
         # histogram of classNames
-        histo = {}        
+        histo = {}
         for g in glyphs:
             count = histo.get(g.get_main_id(), 0)
             histo[g.get_main_id()] = count + 1
-        
-            
+
         for className in histo:
             if histo[className] < rareThreshold:
-                result.add(className)        
+                result.add(className)
         return result
 
-edit_mnn = EditMnn()    
+edit_mnn = EditMnn()
+
 
 class EditCnn(EditingAlgorithm):
     """**edit_cnn** (kNNInteractive *classifier*, int *k* = 0, bool *randomize*)
-    
+
 Hart's *Condensed Nearest Neighbour (CNN)* editing.
 This alorithm is specialized in removing superfluous glyphs - glyphs that do not
-influence the recognition rate - from the classifier to improve its 
-classification speed. Typically glyphs far from the classifiers decision 
+influence the recognition rate - from the classifier to improve its
+classification speed. Typically glyphs far from the classifiers decision
 boundaries are removed.
 
     *classifier*
         The classifier from which to create an edited copy
     *internalK*
-        The k value used internally by the editing algorithm. 0 means, use the 
-        same value as the given classifier (recommended)    
+        The k value used internally by the editing algorithm. 0 means, use the
+        same value as the given classifier (recommended)
     *randomize*
         Because the processing order of the glyphs in the classifier impacts the
         result of this algorithm, the order will be randomized. If reproducable
@@ -229,17 +235,17 @@ boundaries are removed.
 Reference: P.E. Hart: 'The Condensed Nearest Neighbor rule'. *IEEE Transactions on Information Theory*, 14(3):515-516, 1968
 """
     name = "Hart's Condensed Nearest Neighbour (CNN)"
-    args = Args([Int("Internal k", default=0), 
-                 Check("Randomize", default = True)])
+    args = Args([Int("Internal k", default=0),
+                 Check("Randomize", default=True)])
 
-    def __call__(self, classifier, k = 0, randomize = True):
+    def __call__(self, classifier, k=0, randomize=True):
         # special case of empty classifier
         if (not classifier.get_glyphs()):
             return _copyClassifier(classifier)
 
         if k == 0:
             k = classifier.num_k
-        
+
         progress = ProgressFactory("Generating edited CNN classifier...",
                                       len(classifier.get_glyphs()))
 
@@ -248,23 +254,23 @@ Reference: P.E. Hart: 'The Condensed Nearest Neighbor rule'. *IEEE Transactions 
             elem = _randomSetElement(classifier.get_glyphs())
         else:
             elem = classifier.get_glyphs().__iter__().next()
-        
+
         aGlyphs = [elem]
-        a = kNNInteractive(aGlyphs, classifier.features, 
+        a = kNNInteractive(aGlyphs, classifier.features,
                            classifier._perform_splits, k)
         progress.step()
-        
+
         # initialize Grabbag (b) with all other
         b = classifier.get_glyphs().copy()
-        b.remove(aGlyphs[0]);
+        b.remove(aGlyphs[0])
 
         # Classify each glyph in b with a as the classifier
-        # If glyph is misclassified, add it to a, repeat until no elements are 
+        # If glyph is misclassified, add it to a, repeat until no elements are
         # added to a
         changed = True
         while changed == True:
             changed = False
-            # copy needed because iteration through dict is not possible while 
+            # copy needed because iteration through dict is not possible while
             # deleting items from it
             copyOfB = b.copy()
             for glyph in copyOfB:
@@ -279,23 +285,24 @@ Reference: P.E. Hart: 'The Condensed Nearest Neighbor rule'. *IEEE Transactions 
 
 edit_cnn = EditCnn()
 
+
 class EditMnnCnn(EditingAlgorithm):
     """**edit_mnn_cnn** (kNNInteractive *classifier*, int *k* = 0, bool *protectRare*, int *rareThreshold*, bool *randomize*)
 
 Combined execution of Wilson's Modified Nearest Neighbour and Hart's
-Condensed Nearest Neighbour. Combining the algorithms in this order is 
-recommended, because first bad samples are removed to improve the classifiers 
+Condensed Nearest Neighbour. Combining the algorithms in this order is
+recommended, because first bad samples are removed to improve the classifiers
 accuracy, and then the remaining samples are condensed to speed up the classifier
 
 For documentation of the parameters see the independent algorithms"""
     name = "MNN, then CNN"
-    args = Args([Int("Internal k", default = 0),
-                 Check("Protect rare classes", default = True),
-                 Int("Rare class threshold", default = 3),
-                 Check("Randomize", default = True)])
+    args = Args([Int("Internal k", default=0),
+                 Check("Protect rare classes", default=True),
+                 Int("Rare class threshold", default=3),
+                 Check("Randomize", default=True)])
 
-    def __call__(self, classifier, k = 0, protectRare = True,
-                 rareThreshold = 3, randomize = True):
+    def __call__(self, classifier, k=0, protectRare=True,
+                 rareThreshold=3, randomize=True):
         return edit_cnn(edit_mnn(classifier, k, protectRare, rareThreshold),
                    randomize)
 
